36f78e80232d87417fec459b7c7aa18c240d6ba3
Robert Warren <robert@apexai.net>
2025-08-17 17:19:10 -0500

    wip initial setup

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 -- Load core configs
-require 'keymaps'
-require 'options'
-require 'autocommands'
+require 'core.keymaps'
+require 'core.options'
+require 'core.autocommands'
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
@@ -19,44 +19,35 @@ local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
 
 require('lazy').setup({
-  -- 'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
   {
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+    'webhooked/kanso.nvim',
+    priority = 1000,
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'kanso'
     end,
   },
-
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = true } },
-
-  -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-	require 'kickstart.plugins.mason',
-  require 'kickstart.plugins.autopairs',
-  require 'kickstart.plugins.neo-tree',
-  require 'kickstart.plugins.gitsigns',
-  require 'kickstart.plugins.oil',
-  require 'kickstart.plugins.whichkey',
-  require 'kickstart.plugins.telescope',
-  require 'kickstart.plugins.treesitter',
-  require 'kickstart.plugins.mini',
-  require 'kickstart.plugins.lazydev',
-  require 'kickstart.plugins.lsp',
-  require 'kickstart.plugins.conform',
-  require 'kickstart.plugins.blink',
-  require 'kickstart.plugins.render-markdown',
-  -- require 'kickstart.plugins.roslyn',
-  require 'kickstart.plugins.snipe',
-  require 'kickstart.plugins.lualine',
+  require 'plugins.lualine',
+  require 'plugins.debug',
+  require 'plugins.indent_line',
+  require 'plugins.lint',
+  require 'plugins.mason',
+  require 'plugins.autopairs',
+  require 'plugins.neo-tree',
+  require 'plugins.gitsigns',
+  require 'plugins.oil',
+  require 'plugins.whichkey',
+  require 'plugins.telescope',
+  require 'plugins.treesitter',
+  require 'plugins.mini',
+  require 'plugins.lazydev',
+  require 'plugins.lsp',
+  require 'plugins.conform',
+  require 'plugins.blink',
+  require 'plugins.render-markdown',
+  -- require 'plugins.roslyn',
+  require 'plugins.snipe',
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
